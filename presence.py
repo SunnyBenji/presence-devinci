54182e8eeb0ad275361e9de6e530a94017cf7688
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2022-04-27 09:36:47 +0200

    readMe + env variable

--- presence.py
+++ presence.py
@@ -1,17 +1,27 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+emailFile= open("email_env.txt", 'r')
+email = emailFile.read()
+
+passwordFile= open("password_env.txt", 'r')
+password = passwordFile.read()
+
+os.environ['email'] = email
+os.environ['password'] = password
 #lancement
 driver = webdriver.Chrome(executable_path=r"C:\chromedriver.exe")
 driver.maximize_window()
 driver.get('https://www.leonard-de-vinci.net/');
 
 #connexion
-WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login"]'))).send_keys('email@edu.devinci.fr')
+WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login"]'))).send_keys(os.environ['email'])
 WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="btn_next"]'))).click()
-WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="passwordInput"]'))).send_keys('password')
+WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="passwordInput"]'))).send_keys(os.environ['password'])
 WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submitButton"]'))).click()
 
 
---
+++ readMe.md
@@ -0,0 +1,23 @@
+# Foobar
+
+Bonjour tout le Monde, ce script vous permettra de vous connecter et vous mettre présent automatiquement sur le [portail devinci](https://www.leonard-de-vinci.net/) au lancement du script.
+
+### <p style="color: red">/!\Disclaimer/!\ </p>
+Ce script ne fonctionne pas seul, vous devez le lancer à la main et vous évite juste de faire manuellement le processus, ce n'est pas une fraude à la présence.
+
+## Installation
+
+Vous devrez installer le package selenium
+```bash
+pip install selenium
+```
+
+Vous devrez aussi télécharger une version de [chromeDriver](https://chromedriver.chromium.org/downloads) qui correspond à votre version de chrome.
+
+Enfin, vous pouvez créer à la racine du dossier, deux fichiers textes qui contiendront respectivement votre email et votre mot de passe. 
+
+## Contributing
+Les Pull requests sont les bienvenues. Pour les changements majeurs, veuillez d'abord ouvrir une issue que nous puissions en discuter.
+
+## License
+Benjamin Burstein [IIM](https://www.iim.fr/)
